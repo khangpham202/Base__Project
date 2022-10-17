a88148f060d965d541ccaf3de8c7ee2e19102c02
Khang <doangiang2612@gmail.com>
2022-10-18 00:31:28 +0700

    first commit

---
+++ assets/js/modal.js
@@ -0,0 +1,36 @@
+// Script hiển thị modal
+
+const buyBtns = document.querySelectorAll(".js-buy-ticket");
+const modal = document.querySelector(".js-modal");
+const modalContainer = document.querySelector(".js-modal-container");
+const modalClose = document.querySelector(".js-modal-close");
+const form = document.querySelector(".form-login");
+
+// Hàm hiển thị modal mua vé (thêm class open vào modal)
+function showBuyTickets() {
+  modal.classList.add("open");
+}
+
+// Hàm ẩn modal mua vé (gỡ bỏ class open của modal)
+function hideBuyTickets() {
+  modal.classList.remove("open");
+}
+
+// Lặp qua từng thẻ button và nghe hành vi click
+for (const buyBtn of buyBtns) {
+  buyBtn.addEventListener("click", showBuyTickets);
+}
+
+// Nghe hành vi click vào button close
+modalClose.addEventListener("click", hideBuyTickets);
+
+modal.addEventListener("click", hideBuyTickets);
+
+modalContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+
+modal.addEventListener("keyup", (e) => {
+  console.log(e.keyCode);
+  e.keyCode == 27 && modal.classList.remove("open");
+});
